776c8fb0e0739f92be7cde67576f9dbe941c83a1
GabrielUC <gabrielurias100@gmail.com>
2024-08-29 22:18:25 -0600

    initial

---
+++ src/main/java/io/gauc/sv/micro/MicroBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package io.gauc.sv.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBackOfficeApplication.class, args);
+	}
+
+}
